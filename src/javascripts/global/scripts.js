f7dd40a4052262962015efe1becfb99d66c1dc66
Shaun Wong <ChronoXP@hotmail.com>
2018-10-21 20:54:28 +0800

    Theme Updates
    Added Sections: Header, Footer, Hero, Pricing Plans.

--- src/javascripts/global/scripts.js
+++ src/javascripts/global/scripts.js
@@ -1,7 +1,37 @@
 const $ = require('jquery');
 
-$( document ).ready(function() {
-  console.log(`My body is ready`);
+$(document).ready(function() {
 
+  // Dom Ready
+  console.log(`My body is ready`);
+  $("html").addClass("dom-ready");
+
+  // Create Mobile Overlay
+  $(".mobile-menu-overlay").append(`<div class="container"></div`);
+  $("nav").clone().appendTo(".mobile-menu-overlay .container");
+
+  // Set Selected URL
+  var path = window.location.href;
+  $('nav ul a').each(function() {
+    if (this.href === path) {
+      $(this).parent().addClass('selected');
+    }
+  });
+
+  // Mobile Toggle
+  $(document).on('click', '.mobile-menu', function() {
+    $('.hamburger-box').toggleClass('is-active');
+    $('html').toggleClass('mobile-toggle-active');
+  });
+
+  // On Scroll
+  $(window).scroll(function(){
+        if ($(window).scrollTop() >= 71) {
+            $('html').addClass('on-scroll-header');
+        }
+        else {
+            $('html').removeClass('on-scroll-header');
+        }
+    });
 
 });
