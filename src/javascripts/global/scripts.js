169202dfa51ca7c88ebe05c5a3370e9fb4242fff
ARXChrono <ChronoXP@hotmail.com>
2020-01-08 19:03:03 +0800

    clean up codebase

--- src/javascripts/global/scripts.js
+++ src/javascripts/global/scripts.js
@@ -1,37 +1,32 @@
-const $ = require('jquery');
+const $ = require("jquery");
 
 $(document).ready(function() {
-
   // Dom Ready
   console.log(`My body is ready`);
   $("html").addClass("dom-ready");
 
-  // Create Mobile Overlay
-  $(".mobile-menu-overlay").append(`<div class="container"></div`);
-  $("nav").clone().appendTo(".mobile-menu-overlay .container");
-
   // Set Selected URL
   var path = window.location.href;
-  $('nav ul a').each(function() {
+  $("nav ul a").each(function() {
     if (this.href === path) {
-      $(this).parent().addClass('selected');
+      $(this)
+        .parent()
+        .addClass("selected");
     }
   });
 
   // Mobile Toggle
-  $(document).on('click', '.mobile-menu', function() {
-    $('.hamburger-box').toggleClass('is-active');
-    $('html').toggleClass('mobile-toggle-active');
+  $(document).on("click", ".mobile-menu", function() {
+    $(".hamburger-box").toggleClass("is-active");
+    $("html").toggleClass("mobile-toggle-active");
   });
 
   // On Scroll
-  $(window).scroll(function(){
-        if ($(window).scrollTop() >= 71) {
-            $('html').addClass('on-scroll-header');
-        }
-        else {
-            $('html').removeClass('on-scroll-header');
-        }
-    });
-
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 71) {
+      $("html").addClass("on-scroll-header");
+    } else {
+      $("html").removeClass("on-scroll-header");
+    }
+  });
 });
